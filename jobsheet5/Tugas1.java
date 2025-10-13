eb2b492e60eff6a27a47d20034decbf979f6f7ee
RavySanda <ravyadityaa@gmail.com>
2025-10-13 12:50:28 +0700

    Tugas 1

---
+++ jobsheet5/Tugas1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Tugas1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        String pesan = uktLunas 
+            ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA"
+            : "Pembayaran UKT belum lunas\nSilakan lunasi UKT terlebih dahulu";
+
+        System.out.println(pesan);
+
+        sc.close();
+    }
+}
+
+    
